251cef2d626dc281f1c92d0e8c420070f80b91ec
Vaud Keith <vkagong@safaricom.co.ke>
2024-04-17 00:46:40 +0300

    mini-hr

--- chapt_8_exercise/src/main.rs
+++ chapt_8_exercise/src/main.rs
@@ -1,15 +1,18 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io};
 
 
 fn main() {
+    //median and mode
     let mut test_vec = vec![10,20,304,5,43,23,64,64,234,54,6,2,5,4,64];
-
     println!("The media is {}",median(&mut test_vec));
-
     println!("The mode is {}", mode(&test_vec));
 
+    //pig latin
     let mut test_sentence = String::from("first apple is dope");
     print!("Translated to pig latin {}", pig_latin(&mut test_sentence));
+
+    //mini hr
+    mini_hr();
 }
 
 fn median(numbers: &mut Vec<i32>) -> i32{
@@ -56,4 +59,41 @@ fn pig_latin(sentence: &mut String) -> String{
     new_sentence
 }
 
+fn mini_hr(){
+    let mut command = String::new();
+    let mut tokens: Vec<String> = Vec::new();
+
+    let mut sales_employees: Vec<String> = Vec::new();
+    let mut engineering_employees: Vec<String> = Vec::new();
+
+    println!("\n\nWelcome to mini hr\n\nSample command(Add [Username] to [department (Sales/Engineering)])\nType show to display list");
+    get_user_input(&mut command);
+
+    let mut i = 0;
+
+    for token in command.split_whitespace(){
+        tokens.insert(i, String::from(token));
+        i+=1;
+    }
+
+    if tokens[3].eq("Sales") {
+        sales_employees.push(String::from(&tokens[1]));
+    }else if tokens[3].eq("Engineering") {
+        engineering_employees.push(String::from(&tokens[1]));
+    }else{
+        println!("Invalid department")
+    }
+
+
+    //print
+    sales_employees.sort();
+    engineering_employees.sort();
+    println!("Sales employees:\n{:?}",sales_employees);
+    println!("Engineering employees:\n{:?}",engineering_employees);
+    
+}
+
+fn get_user_input(field: &mut String) {
+    io::stdin().read_line(field).expect("Failed to read input");
+}
 
